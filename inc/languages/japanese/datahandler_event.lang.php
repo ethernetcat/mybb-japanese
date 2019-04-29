3c29be8df8987ee924be9ac33019fbdd03aee638
SNAKE <ethernetcat@protonmail.com>
2019-04-30 01:50:17 +0900

    Create datahandler_event.lang.php

---
+++ inc/languages/japanese/datahandler_event.lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['eventdata_missing_name'] = "イベント名がありません。イベント名を入力してください。";
+$l['eventdata_missing_description'] = "イベントの説明がありません。イベントの説明を入力してください。";
+
+$l['eventdata_invalid_start_date'] = "入力したイベント開始日が無効です。入力した日付が有効であるか確認して下さい。";
+$l['eventdata_invalid_start_year'] = "イベントは現在から5年以内にしか作れません。リストから適切な開始年を選択してください。";
+$l['eventdata_invalid_start_month'] = "入力した開始月は有効な月ではありません。有効な開始月を入力して下さい。";
+
+$l['eventdata_invalid_end_date'] = "入力したイベント終了日が無効です。入力した日付が有効であるか確認して下さい。";
+$l['eventdata_invalid_end_year'] = "イベントは現在から5年以内にしか作れません。リストから適切な終了年を選択してください。";
+$l['eventdata_invalid_end_month'] = "入力した開始月は有効な月ではありません。有効な終了月を入力して下さい。";
+$l['eventdata_invalid_end_day'] = "入力したイベント終了日が無効です。入力した日付は、恐らく今月の日数よりも大きいです。";
+
+$l['eventdata_cant_specify_one_time'] = "イベント開始時刻を指定する場合は、終了時刻を入力する必要があります。";
+$l['eventdata_start_time_invalid'] = "入力した開始時刻が無効です。有効な入力例は12am、12:01am、00:01です。";
+$l['eventdata_end_time_invalid'] = "入力した終了時刻が無効です。有効な入力例は12am、12:01am、00:01です。";
+$l['eventdata_invalid_timezone'] = "このイベントに選択したタイムゾーンは無効です。";
+$l['eventdata_end_in_past'] = "イベントの終了日または終了時刻は、開始日または開始時刻よりも前です。";
+
+$l['eventdata_only_ranged_events_repeat'] = "範囲指定されたイベント（開始日と終了日のあるイベント）のみ繰り返すことが出来ます。";
+$l['eventdata_invalid_repeat_day_interval'] = "無効な日の繰り返し間隔を入力しました。";
+$l['eventdata_invalid_repeat_week_interval'] = "無効な週の繰り返し間隔を入力しました。";
+$l['eventdata_invalid_repeat_weekly_days'] = "イベントが平日を選択していません。";
+$l['eventdata_invalid_repeat_month_interval'] = "無効な月の繰り返し間隔を入力しました。";
+$l['eventdata_invalid_repeat_year_interval'] = "無効な年の繰り返し間隔を入力しました。";
+$l['eventdata_event_wont_occur'] = "開始時刻と終了時刻をイベントの繰り返しとともに設定すると、このイベントが発生しません。";
+
+$l['eventdata_no_permission_private_event'] = "プライベートイベントを作成する権限がありません。";
