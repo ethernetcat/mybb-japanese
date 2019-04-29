375b3da11ecfdd0666247045aec112efbe6e09e4
SNAKE <ethernetcat@protonmail.com>
2019-04-30 00:28:47 +0900

    Create japanese.php

---
+++ inc/languages/japanese.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "日本語(Japanese)";
+
+// The author of the language
+$langinfo['author'] = "snake0053";
+
+// The language authors website
+$langinfo['website'] = "https://github.com/snake0053/mybb-japanese";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1820";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 0;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "ja";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
