ba2391aac47aeda3ab09fd5d1a9987a35969f86d
SNAKE <ethernetcat@protonmail.com>
2019-04-30 01:23:16 +0900

    Create calendar.lang.php

---
+++ inc/languages/japanese/calendar.lang.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['nav_calendar'] = "カレンダー";
+$l['nav_viewevent'] = "イベントを見る";
+$l['nav_dayview'] = "日を見る";
+$l['nav_addevent'] = "イベントを追加";
+$l['nav_editevent'] = "イベントを編集";
+$l['nav_move_event'] = "イベントを移動";
+
+$l['calendar_disabled'] = "カレンダー機能は管理者によって無効にされているため使用できません。";
+
+$l['alt_month_1'] = '1月';
+$l['alt_month_2'] = '2月';
+$l['alt_month_3'] = '3月';
+$l['alt_month_4'] = '4月';
+$l['alt_month_5'] = '5月';
+$l['alt_month_6'] = '6月';
+$l['alt_month_7'] = '7月';
+$l['alt_month_8'] = '8月';
+$l['alt_month_9'] = '9月';
+$l['alt_month_10'] = '10月';
+$l['alt_month_11'] = '11月';
+$l['alt_month_12'] = '12月';
+
+$l['add_private_event'] = "プライベートイベントを追加";
+$l['add_public_event'] = "公開イベントを追加";
+
+$l['calendar'] = "カレンダー";
+
+$l['jump_month'] ="月に移動：";
+
+$l['birthdays_on_day'] = "{1}人の誕生日";
+$l['birthdayhidden'] = "非表示";
+
+$l['event'] = "イベント";
+$l['events'] = "イベント";
+$l['add_event'] = "イベントを追加";
+$l['event_date'] = "イベント日：";
+$l['event_recurring_start_date'] = "開始日：";
+$l['event_recurring_end_date'] = "終了日：";
+$l['event_recurring_repeat_days'] = "イベントの繰り返し：";
+$l['event_name'] = "イベント名：";
+$l['event_details'] = "イベント詳細：";
+$l['event_options'] = "オプション：";
+$l['private_option'] = "<b>非公開</b> あなただけがこのイベントを観覧できます。（登録ユーザーのみ）";
+$l['delete_option'] = "<b>削除：</b> このイベントを削除";
+$l['post_event'] = "イベントを送信";
+$l['day_view'] = "日を見る";
+$l['birthday'] = "誕生日";
+$l['birthdays'] = "誕生日";
+$l['event_author'] = "イベント作成者：";
+$l['edit_event'] = "イベント編集";
+$l['view_event'] = "イベントを見る";
+$l['no_events'] = "この日にはイベントが関連付けられていません。<p><a href=\"calendar.php?action=addevent&amp;calendar={1}&amp;day={2}&amp;month={3}&amp;year={4}\">イベントを送信</a>してください。</p>";
+$l['years_old'] = "{1}歳";
+$l['alt_edit'] = "このイベントを編集";
+$l['alt_delete'] = "このイベントを削除";
+$l['moderator_options'] = "モデレータオプション";
+$l['approve_event'] = "イベントを承認";
+$l['unapprove_event'] = "イベントを却下";
+$l['move_event'] = "イベントを移動";
+$l['repeats_every_day'] = "毎日繰り返す";
+$l['repeats_every_x_days'] = "{1}日ごとに繰り返す";
+$l['repeats_on_weekdays'] = "月曜から金曜まで繰り返す";
+$l['every_week_on_days'] = "毎週{1}に繰り返す";
+$l['every_week'] = "毎週繰り返す";
+$l['every_x_weeks_on_days'] = "{1}週間ごとの{2}に<br />繰り返す";
+$l['every_x_weeks'] = "{1}週間ごとに繰り返す";
+$l['every_month_on_day'] = "毎月{1}日に<br />繰り返す";
+$l['every_x_months_on_day'] = "{2}ヶ月ごとの{1}日に<br />繰り返す";
+$l['every_month_on_weekday'] = "毎月{1} {2}に<br />繰り返す";
+$l['every_x_months_on_weekday'] = "{3}ヶ月ごとの{1} {2}に<br />繰り返す";
+$l['weekday_occurance_1'] = "第1";
+$l['weekday_occurance_2'] = "第2";
+$l['weekday_occurance_3'] = "第3";
+$l['weekday_occurance_4'] = "第4";
+$l['weekday_occurance_last'] = "最終";
+$l['every_year_on_day'] = "毎年{1} {2}に繰り返す";
+$l['every_x_years_on_day'] = "{3}年ごとの{1} {2}に繰り返す";
+$l['every_year_on_weekday'] = "毎年{3}の{1} {2}に繰り返す";
+$l['every_x_year_on_weekday'] = "{4}年ごとの{3}の{1} {2}に繰り返す";
+$l['delete_event'] = "イベントを削除";
+$l['delete_q'] = "削除しますか？";
+$l['delete_1'] = "このイベントを削除するには、左側のチェックボックスをオンにしてから右側のボタンをクリックします。";
+$l['delete_2'] = "<b>注意：</b> この操作は取り消せません。";
+$l['delete_now'] = "今すぐ削除";
+$l['delete_no_checkbox'] = "\"削除\" チェックボックスをオンにしていないため、イベントは削除されませんでした。";
+$l['jump_to_calendar'] = "カレンダーに移動：";
+$l['select_calendar'] = "カレンダー：";
+$l['type_single'] = "１日限りのイベント";
+$l['type_ranged'] = "範囲指定もしくは定期的なイベント";
+$l['enter_time'] = "時間：";
+$l['start_time'] = "開始：";
+$l['end_time'] = "終了：";
+$l['timezone'] = "タイムゾーン：";
+$l['ignore_timezone'] = "<strong>タイムゾーンを無視：</strong> このイベントは各ユーザーのタイムゾーンを使用します。";
+$l['repeats'] = "繰り返し：";
+$l['does_not_repeat'] = "繰り返さない";
+$l['repeats_daily'] = "毎日";
+$l['repeats_weekdays'] = "平日";
+$l['repeats_weekly'] = "毎週";
+$l['repeats_every'] = "繰り返し";
+$l['day_or_days'] = "日";
+$l['week_or_weeks_on'] = "週";
+$l['repeats_monthly'] = "月";
+$l['repeats_yearly'] = "年";
+$l['repeats_on_day'] = "";
+$l['of_every'] = "日に";
+$l['month_or_months'] = "ヶ月ごと";
+$l['repeats_on_the'] = "";
+$l['day_of_every'] = "日ごとに繰り返す";
+$l['repeats_on'] = "";
+$l['every'] = "に";
+$l['year_or_years'] = "年ごとに繰り返す";
+$l['of'] = "の";
+$l['move_to_calendar'] = "カレンダーを移動";
+$l['weekly_overview'] = "週間で表示";
+$l['previous_week'] = "前の週";
+$l['next_week'] = "次の週";
+$l['first'] = '第1';
+$l['second'] = '第2';
+$l['third'] = '第3';
+$l['fourth'] = '第4';
+$l['last'] = '最終';
+$l['all_day'] = "全ての日";
+$l['starts'] = "開始： ";
+$l['finishes'] = "終了： ";
+
+$l['error_incorrectday'] = "あなたが入力した日付は存在しません。もう一度やり直してください。";
+$l['error_invalidevent'] = "指定したイベントは無効であるか存在しません。";
+$l['invalid_calendar'] = "指定されたカレンダーは存在しません。正しいページにアクセスしてよろしいですか？";
+$l['redirect_eventdeleted'] = "イベントは削除されました。<br />カレンダーに戻ります。";
+$l['redirect_eventupdated'] = "イベントは正常に更新されました。<br />イベントに戻ります。";
+$l['redirect_eventadded'] = "イベントは正常に追加されました。<br />追加したイベントに移動します。";
+$l['redirect_eventadded_moderation'] = "イベントは正常に追加されましたが、モデレータによる承認が必要です。<br />カレンダーに戻ります。";
+$l['redirect_eventunapproved'] = "イベントは却下されました。<br />イベントに戻ります。";
+$l['redirect_eventapproved'] = "イベントは承認されました。<br />承認されたイベントに移動します。";
+$l['redirect_eventmoved'] = "イベントは正常に移動されました。<br />イベントに戻ります。";
