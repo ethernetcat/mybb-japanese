83b23549f551c4466082b6770c602d932ad45c58
SNAKE <ethernetcat@protonmail.com>
2019-04-30 00:31:53 +0900

    Create announcements.lang.php

---
+++ inc/languages/japanese/announcements.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['nav_announcements'] = "フォーラムのお知らせ";
+$l['announcements'] = "お知らせ";
+$l['forum_announcement'] = "フォーラムのお知らせ：{1}";
+$l['error_invalidannouncement'] = "指定されたお知らせは無効です。";
+
+$l['announcement_edit'] = "このお知らせを編集";
+$l['announcement_qdelete'] = "このお知らせを削除";
+$l['announcement_quickdelete_confirm'] = "このお知らせを削除してもよろしいですか？";
