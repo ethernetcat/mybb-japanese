157a15ecc9434e972d0ca8e03c40a98a4d542af4
SNAKE <ethernetcat@protonmail.com>
2019-04-30 01:37:31 +0900

    Create customhelpsections.lang.php

---
+++ inc/languages/japanese/customhelpsections.lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+/*
+ * カスタムヘルプセクション 翻訳フォーマット
+ *
+ * // ヘルプセクション {sid}
+ * $l['s{sid}_name'] = "セクション名";
+ * $l['s{sid}_desc'] = "セクション概要";
+ */
