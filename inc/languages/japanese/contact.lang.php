c59a44f69633c0fdc42ed1a8590ca162fb619d07
SNAKE <ethernetcat@protonmail.com>
2019-04-30 01:34:57 +0900

    Create contact.lang.php

---
+++ inc/languages/japanese/contact.lang.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['contact'] = 'お問い合わせ';
+$l['contact_no_message'] = 'メッセージが入力されていません。';
+$l['contact_no_subject'] = '件名が入力されていません。';
+$l['contact_no_email'] = '有効なメールアドレスを入力してください。';
+$l['contact_success_message'] = 'メッセージは管理者に送信されました。';
+$l['contact_subject'] = '件名';
+$l['contact_subject_desc'] = 'メッセージの要約を入力してください。';
+$l['contact_message'] = 'メッセージ';
+$l['contact_message_desc'] = 'お問い合わせ内容を詳細に説明してください。';
+$l['contact_email'] = 'メールアドレス';
+$l['contact_email_desc'] = '私達が連絡できるようにするため返信用メールアドレスを入力して下さい。';
+$l['contact_send'] = '送信';
+$l['subject_too_long'] = '件名が長すぎます。 件名は{1}文字より短く入力してください。（現在{2}文字）';
+$l['message_too_short'] = 'メッセージが短すぎます。 メッセージは{1}文字以上で入力してください。（現在{2}文字）';
+$l['message_too_long'] = 'メッセージが長すぎます。メッセージは{1}文字以内で入力してください。（現在{2}文字）';
+
+$l['error_stop_forum_spam_spammer'] = '申し訳ありませんが、{1}は既知のスパムに一致したため、送信がブロックされました。';
+$l['error_stop_forum_spam_fetching'] = '申し訳ありませんが、スパムデータベースの確認に失敗しました。データベースにアクセスできなかった可能性があります。しばらく待ってからもう一度やり直して下さい。';
