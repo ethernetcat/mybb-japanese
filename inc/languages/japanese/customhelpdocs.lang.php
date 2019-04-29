9fd6aebdeb111553034d438eb61e5dd5d8c32884
SNAKE <ethernetcat@protonmail.com>
2019-04-30 01:36:49 +0900

    Create customhelpdocs.lang.php

---
+++ inc/languages/japanese/customhelpdocs.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+/*
+ * カスタムヘルプドキュメント 翻訳フォーマット
+ *
+ * // ヘルプドキュメント {hid}
+ * $l['d{hid}_name'] = "ドキュメント名";
+ * $l['d{hid}_desc'] = "ドキュメント概要";
+ * $l['d{hid}_document'] = "ドキュメントテキスト";
+ */
