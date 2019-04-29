6bf656213f3af50fb8220b3d76ba65b4cb42858b
SNAKE <ethernetcat@protonmail.com>
2019-04-30 00:39:25 +0900

    Create archive.lang.php

---
+++ inc/languages/japanese/archive.lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['archive_fullversion'] = 'フルバージョン:';
+$l['archive_replies'] = '返信';
+$l['archive_reply'] = '返信';
+$l['archive_pages'] = 'ページ：';
+$l['archive_note'] = 'あなたは現在、私達のコンテンツの簡略版を見ています。適切なフォーマットで<a href=\"{1}\">フルバージョン</a>を表示します。';
+$l['archive_nopermission'] = '申し訳ありませんが、このリソースにアクセスする権限がありません。';
+$l['error_nothreads'] = 'このフォーラムには、現在スレッドがありません。';
+$l['error_nopermission'] = 'このフォーラムのスレッドを表示する権限がありません。';
+$l['error_unapproved_thread'] = 'このスレッドは承認されていません。スレッドの内容を表示するには<a href=\"{1}\">フルバージョン</a>をご覧ください。';
+$l['archive_not_found'] = '要求されたページは見つかりませんでした。';
+$l['error_mustlogin'] = 'この掲示板はすべてのユーザーにログインを要求します。';
