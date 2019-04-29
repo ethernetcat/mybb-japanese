c80a90e4fae7351230651e537c8bc01fbbce11a9
SNAKE <ethernetcat@protonmail.com>
2019-04-30 02:04:59 +0900

    Create index.lang.php

---
+++ inc/languages/japanese/index.lang.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['boardstats'] = '掲示板の統計';
+$l['new_posts'] = '新着投稿のあるフォーラム';
+$l['no_new_posts'] = '新着投稿のないフォーラム';
+$l['forum_closed'] = 'フォーラムは終了しました';
+$l['forum_unapproved_posts_count'] = '拒否された記事が現在{1}個あります。';
+$l['forum_unapproved_post_count'] = '拒否された記事が現在1個あります。';
+$l['forum_unapproved_threads_count'] = '拒否されたスレッドが現在{1}個あります。';
+$l['forum_unapproved_thread_count'] = '拒否されたスレッドが現在1個あります。';
+$l['forumstats'] = 'フォーラムの統計';
+$l['todays_birthdays'] = '今日の誕生日';
+$l['birthdayhidden'] = '非表示';
+$l['quick_login'] = 'クイックログイン：';
+$l['index_logout'] = 'ログアウト';
+$l['private_messages'] = 'プライベートメッセージ';
+$l['markread'] = 'すべてのフォーラムを既読にする';
+$l['forumteam'] = 'フォーラムチーム';
+$l['pms_new'] = '最後のログイン以降、{1}個の新しいメッセージがあります。';
+$l['pms_unread_total'] = '（フォルダに{1}個の未読メッセージ、合計{2}個のメッセージがあります。）';
+$l['stats_posts_threads'] = '私達のメンバーは{2}個のスレッドで合計{1}個の投稿をしました。';
+$l['stats_numusers'] = '現在{1}人がメンバー登録しています。';
+$l['stats_newestuser'] = '最新のメンバー <b>{1}</b>を歓迎して下さい。';
+$l['stats_mostonline'] = '最大同時オンラインは{2}{3}の{1}人でした。';
+$l['whos_online'] = 'オンラインユーザー';
+$l['complete_list'] = '全員を表示';
+$l['online_online_plural'] = 'ユーザー';
+$l['online_online_singular'] = 'ユーザー';
+$l['online_member_plural'] = '登録メンバー';
+$l['online_member_singular'] = '登録メンバー';
+$l['online_anon_plural'] = 'は';
+$l['online_anon_singular'] = 'は';
+$l['online_guest_plural'] = 'ゲスト';
+$l['online_guest_singular'] = 'ゲスト';
+$l['online_note'] = '過去{3}分間に{1}{2}がアクティブになりました。（{5}{4}人中、非表示{7}{6}人、{9}は{8}人）';
+$l['subforums'] = 'サブフォーラム：';
+$l['board_stats_link_separator'] = ' | ';
