398700b2808b8ba03b54df291787704a69e2d3cf
SNAKE <ethernetcat@protonmail.com>
2019-05-08 02:51:12 +0900

    Create xmlhttp.lang.php

---
+++ inc/languages/japanese/xmlhttp.lang.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['no_new_subject'] = '新しい件名が入力されていません。';
+$l['post_moderation'] = '投稿は現在モデレート中です。';
+$l['thread_moderation'] = 'スレッドは現在モデレート中です。';
+$l['post_doesnt_exist'] = '指定された投稿は存在しません。';
+$l['thread_doesnt_exist'] = '指定されたスレッドは存在しません。';
+$l['thread_closed_edit_subjects'] = 'このスレッドは締め切られているため、件名を編集できません。';
+$l['no_permission_edit_subject'] = 'このスレッドの件名を編集する権限がありません。';
+$l['thread_closed_edit_message'] = 'このスレッドは締め切られているため、その中のメッセージは編集できません。';
+$l['no_permission_edit_post'] = 'このメッセージを編集する権限がありません。';
+$l['edit_time_limit'] = '投稿後{1}分間だけ編集できます。';
+$l['postbit_edited'] = 'この投稿は{1}に編集されました。編集者';
+$l['postbit_editreason'] = '理由を編集';
+$l['save_changes'] = '変更を保存';
+$l['cancel_edit'] = '編集をキャンセル';
+$l['answer_valid_not_exists'] = '質問が存在しません。';
+$l['captcha_not_exists'] = '更新しようとしている画像認証の画像が存在しません。';
+$l['captcha_valid_not_exists'] = '確認しようとしている画像認証の画像が存在しません。';
+$l['captcha_does_not_match'] = '入力された画像認証コードが正しくありません。画像に表示された通りにコードを入力してください。';
+$l['captcha_matches'] = '入力された画像認証コードで認証されました。';
+$l['answer_does_not_match'] = '入力した答えが正しくありません。';
+$l['banned_username'] = '入力されたユーザー名は管理者によって許可されていません。';
+$l['banned_characters_username'] = 'ユーザー名に１つ以上の無効な文字が含まれています。';
+$l['complex_password_fails'] = 'パスワードには大文字、小文字、数字が含まれてる必要があります。';
+$l['username_taken'] = '{1}はすでに他のメンバーによって登録されています。';
+$l['username_available'] = '{1}は利用可能です。';
+$l['invalid_username'] = '{1}は登録メンバーのユーザー名ではありません。';
+$l['valid_username'] = '{1}は有効な紹介元です。';
+$l['buddylist_error'] = 'バディリストにバディがいません。これを使う前にバディを追加してください。';
+$l['close'] = '閉じる';
+$l['select_buddies'] = 'バディを選択';
+$l['select_buddies_desc'] = '１人以上のバディを受信者として追加するには、下から選択してOKをクリックして下さい。';
+$l['selected_recipients'] = '選択した受信者';
+$l['ok'] = 'OK';
+$l['cancel'] = 'キャンセル';
+$l['online'] = 'オンライン';
+$l['offline'] = 'オフライン';
+$l['edited_post'] = '編集済みの投稿';
+$l['usergroup'] = 'ユーザーグループ';
