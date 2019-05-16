e9feebc122c29980a25a31982be27204b4a01095
SNAKE <ethernetcat@protonmail.com>
2019-05-16 15:08:21 +0900

    Create datahandler_login.lang.php

---
+++ inc/languages/japanese/datahandler_login.lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['logindata_invalidpwordusername'] = "無効なユーザー名とパスワードの組み合わせを入力しました。<br><br>パスワードを忘れた場合は<a href=\"member.php?action=lostpw\">新しいパスワード</a>を取得してください。";
+$l['logindata_invalidpwordusernameemail'] = "無効なメールアドレスとパスワードの組み合わせを入力しました。<br><br>パスワードを忘れた場合は<a href=\"member.php?action=lostpw\">新しいパスワード</a>を取得してください。";
+$l['logindata_invalidpwordusernamecombo'] = "無効なユーザー名とパスワードまたは電子メールとパスワードの組み合わせを入力しました。<br><br>パスワードを忘れた場合は<a href=\"member.php?action=lostpw\">新しいパスワード</a>を取得してください。";
+
+$l['logindata_regimageinvalid'] = "入力した確認コードが正しくありません。画像に表示されるコードを正確に入力してください。";
+$l['logindata_regimagerequired'] = "ログインを続行するには、確認コードを入力してください。画像に表示されるコードを正確に入力してください。";
