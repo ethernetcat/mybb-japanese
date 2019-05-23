2275a170bb85bb42438cee1810b5c2b23a764d1d
SNAKE <ethernetcat@protonmail.com>
2019-05-23 12:05:33 +0900

    Create datahandler_post.lang.php

---
+++ inc/languages/japanese/datahandler_post.lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['postdata_invalid_user_id'] = 'ユーザーIDが存在しません。有効なユーザーIDを入力してください。';
+$l['postdata_firstpost_no_subject'] = 'スレッドに件名がありません。件名を入力してください。';
+$l['postdata_missing_subject'] = '件名がありません。件名を入力してください。';
+$l['postdata_missing_message'] = '本文がありません。本文を入力してください。';
+$l['postdata_message_too_long'] = '本文が長すぎます。 {1}文字より短くしてください。（現在{2}文字）';
+$l['postdata_message_too_short'] = '本文が短すぎます。{1}文字以上入力してください。';
+$l['postdata_subject_too_long'] = '件名が長すぎます。 {1}文字より短くしてください。（現在{2}文字）';
+$l['postdata_banned_username'] = '入力したユーザー名は登録することができません。別のユーザー名を入力してください。';
+$l['postdata_bad_characters_username'] = '入力したユーザー名に不正な文字が含まれています。別のユーザー名を入力してください。';
+$l['postdata_invalid_username_length'] = '入力したユーザー名の長さが無効です。ユーザー名は{1}文字から{2}文字の間で入力してください。';
+$l['postdata_username_exists'] = '入力したユーザー名は既に存在します。別のユーザー名を入力してください。';
+$l['postdata_post_flooding'] = '前回の投稿からの間隔が短すぎます。前回の投稿から{1}秒待ってください。';
+$l['postdata_post_flooding_one_second'] = '前回の投稿からの間隔が短すぎます。前回の投稿から1秒待ってください。';
+$l['postdata_too_many_images'] = '投稿に{1}個の画像が添付されていますが、投稿あたり{2}個に制限されています。添付画像を減らしてください。';
+$l['postdata_too_many_videos'] = '投稿に{1}個の動画が添付されていますが、投稿あたり{2}個に制限されています。添付動画を減らしてください。';
+$l['postdata_invalid_prefix'] = '選択されたプレフィックスは無効です。有効なプレフィックスを選択してください。';
+$l['postdata_require_prefix'] = 'このフォーラムではスレッドプレフィックスを設定する必要があります。有効なプレフィックスを選択してください。';
+
+$l['thread_closed'] = "スレッドクローズ";
+$l['thread_opened'] = "スレッドオープン";
+$l['thread_stuck'] = "スレッド固定";
+$l['thread_unstuck'] = "スレッド固定解除";
