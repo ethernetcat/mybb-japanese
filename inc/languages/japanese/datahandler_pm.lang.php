8b40a067707b0a59553e462e3a53d4b7e452e33d
SNAKE <ethernetcat@protonmail.com>
2019-05-23 11:33:27 +0900

    Create datahandler_pm.lang.php

---
+++ inc/languages/japanese/datahandler_pm.lang.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['pmdata_too_long_subject'] = 'プライベートメッセージの件名が長すぎます。件名を短くしてください。';
+$l['pmdata_code_no_subject'] = '[件名なし]';
+$l['pmdata_missing_subject'] = '件名が入力されていません。件名を入力してください。';
+$l['pmdata_missing_message'] = '本文が入力されていません。本文を入力してください。';
+$l['pmdata_message_too_long'] = '本文が長すぎます。{1}文字より短くしてください。（現在{2}文字）';
+$l['pmdata_invalid_recipients'] = "宛先に1人以上の未登録ユーザーが指定されています。無効な宛先：{1}';
+$l['pmdata_no_recipients'] = "宛先が指定されていません。「to」に少なくとも1人のユーザー名が必要です。';
+$l['pmdata_too_many_recipients'] = "一度に最大{1}人のユーザーに送信することが許可されています。";
+$l['pmdata_recipient_is_ignoring'] = "無視リスト入れられているため、{1}に送信できません。";
+$l['pmdata_recipient_has_buddy_only'] = "バディリストに入っていないため、{1}に送信できません。";
+$l['pmdata_recipient_pms_disabled'] = '{1}はプライベートメッセージを無効にしています。このユーザーにメッセージを送信することはできません。';
+$l['pmdata_recipient_reached_quota'] = '{1}はプライベートメッセージの容量が上限に達したため、メッセージを送信できませんでした。';
+
+$l['pmdata_pm_flooding'] = '前回のメッセージ送信からの間隔が短すぎます。前回の送信から{1}秒待ってください。';
+$l['pmdata_pm_flooding_one_second'] = '前回のメッセージ送信からの間隔が短すぎます。前回の送信から1秒待ってください。';
