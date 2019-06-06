b4581dd5e8e6adc81cd0d3a2b88d038976cc22c7
SNAKE <ethernetcat@protonmail.com>
2019-06-06 16:05:21 +0900

    Create report.lang.php

---
+++ inc/languages/japanese/report.lang.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['report_reason'] = "通報理由：";
+$l['report_to_mod'] = "コンテンツをモデレータに通報";
+$l['close_window'] = "ウィンドウを閉じる";
+
+// Content types
+$l['report_content'] = "コンテンツを通報";
+$l['report_reason_post'] = "投稿を通報";
+$l['report_reason_profile'] = "プロフィールを通報";
+$l['report_reason_reputation'] = "評価を通報";
+
+// Content reasons
+$l['report_reason_rules'] = "フォーラムルールに違反";
+$l['report_reason_bad'] = "不適切なコンテンツ";
+$l['report_reason_spam'] = "スパムコンテンツ";
+$l['report_reason_wrong'] = "間違ったフォーラム";
+$l['report_reason_other'] = "その他";
+
+$l['success_report_voted'] = "このコンテンツを通報していただきありがとうございます。<br />フォーラムチームのメンバーがすぐに通報を確認します。";
+
+$l['error_report_length'] = "通報の詳細な理由を入力してください。";
+$l['error_invalid_report'] = "{1}は存在しないか、通報できません。";
+$l['error_invalid_report_reason'] = "選択された理由は無効です。";
+$l['error_comment_required'] = "選択した理由には追加のコメントが必要です。";
+$l['error_report_duplicate'] = "このコンテンツは他のメンバーから既に報告されています。<br />このコンテンツを通報する場合は下からできます。";
+$l['report_reason_other_description'] = "なぜこのコンテンツを通報しているのかについて、さらに多くの情報を提供してください。";
