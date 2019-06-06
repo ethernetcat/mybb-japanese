62bc4dcf8463ec388348ddf589bb680119140a07
SNAKE <ethernetcat@protonmail.com>
2019-06-06 15:29:02 +0900

    Create syndication.lang.php

---
+++ inc/languages/japanese/syndication.lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['all_forums'] = "全フォーラム";
+$l['forum'] = "フォーラム：";
+$l['posted_by'] = "投稿者：";
+$l['on'] = " ";
+$l['portal'] = "ポータル";
