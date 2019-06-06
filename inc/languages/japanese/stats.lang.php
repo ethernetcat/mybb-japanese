cbc4f63e6229988f80069d84e4c3c7cf7b23abda
SNAKE <ethernetcat@protonmail.com>
2019-06-06 15:27:07 +0900

    Create stats.lang.php

---
+++ inc/languages/japanese/stats.lang.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['nav_stats'] = "掲示板の統計";
+$l['stats_disabled'] = "管理者によって無効にされているため、統計ページを閲覧できません。";
+
+$l['board_stats'] = "掲示板の統計";
+$l['none'] = "なし";
+$l['totals'] = "合計";
+$l['averages'] = "平均";
+$l['posts'] = "投稿数：";
+$l['threads'] = "スレッド数：";
+$l['members'] = "メンバー数：";
+$l['ppd'] = "一日当たりの投稿数：";
+$l['tpd'] = "一日当たりのスレッド数：";
+$l['mpd'] = "一日当たりのメンバー数：";
+$l['ppm'] = "メンバー当たりの投稿数：";
+$l['tpm'] = "メンバー当たりのスレッド数";
+$l['rpt'] = "スレッド当たりの返信数：";
+$l['no_posts'] = "なし";
+$l['general'] = "全般";
+$l['newest_member'] = "新規メンバー：";
+$l['members_posted'] = "投稿したメンバー：";
+$l['todays_top_poster'] = "今日のトップ投稿者：<b>{1}</b> (<b>{2}</b>投稿)";
+$l['popular_forum'] = "最も人気のあるフォーラム： <b>{1}</b> (<b>{2}</b>投稿、<b>{3}</b>スレッド)";
+$l['most_popular'] = "最も人気&hellip;";
+$l['most_replied_threads'] = "最も返信が多いスレッド";
+$l['most_viewed_threads'] = "最も見られたスレッド";
+$l['not_enough_info_stats'] = "申し訳ありませんが、統計を生成するのに十分な情報がありません。統計を生成するには最低でも１人のメンバーを１つのスレッドが存在している必要があります。";
+$l['replies'] = "返信";
+$l['views'] = "ビュー";
+$l['top_referrer'] = "トップ紹介者： <strong>{1}</strong> (<strong>{2}</strong>人紹介)";
