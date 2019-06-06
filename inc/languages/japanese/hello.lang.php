740a6ec010e77935d66a5766cf4214551991f745
SNAKE <ethernetcat@protonmail.com>
2019-06-06 15:13:01 +0900

    Create hello.lang.php

---
+++ inc/languages/japanese/hello.lang.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['hello'] = 'Hello World!';
+$l['hello_add'] = '追加';
+$l['hello_add_message'] = 'メッセージを追加';
+$l['hello_empty'] = 'メッセージは見つかりませんでした。';
+$l['hello_message_empty'] = 'メッセージを空にすることはできません。';
+$l['hello_done'] = '新しいメッセージの追加に成功しました。';
