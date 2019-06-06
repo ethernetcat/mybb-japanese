ed1885272d211ff0bc7c7a7f3abf91f15a8836a6
SNAKE <ethernetcat@protonmail.com>
2019-06-06 15:45:39 +0900

    Create sendthread.lang.php

---
+++ inc/languages/japanese/sendthread.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['nav_sendthread'] = "スレッドをフレンドに送信";
+
+$l['send_thread'] = "フレンドに送信";
+$l['recipient'] = "受取人：";
+$l['recipient_note'] = "ここにフレンドのメールアドレスを入力してください。";
+$l['subject'] = "件名：";
+$l['message'] = "本文：";
+$l['error_nosubject'] = "スレッドをフレンドに送信するには件名を入力する必要があります。";
+$l['error_nomessage'] = "スレッドをフレンドに送信するには本文を入力する必要があります。";
