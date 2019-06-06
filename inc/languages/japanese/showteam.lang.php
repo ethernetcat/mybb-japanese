591ff7d6e0637147c1e18c05808a4ed537f1c74d
SNAKE <ethernetcat@protonmail.com>
2019-06-06 15:34:58 +0900

    Create showteam.lang.php

---
+++ inc/languages/japanese/showteam.lang.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['nav_showteam'] = "フォーラムチーム";
+$l['forum_team'] = "フォーラムチーム";
+$l['moderators'] = "モデレータ";
+$l['username'] = "ユーザー名";
+$l['lastvisit'] = "最終訪問";
+$l['email'] = "メール";
+$l['pm'] = "PM";
+$l['mod_forums'] = "フォーラム";
+$l['mod_groups'] = "グループ";
+$l['online'] = "オンライン";
+$l['offline'] = "オフライン";
+$l['away'] = "不在";
+
+$l['group_leaders'] = "グループリーダー";
+$l['group_members'] = "メンバー";
+
+$l['no_members'] = "No members in this group";
+$l['showteam_disabled'] = "フォーラムチームリストは管理者によって無効にされています。";
+$l['error_noteamstoshow'] = "表示できるフォーラムスタッフがいません。";
