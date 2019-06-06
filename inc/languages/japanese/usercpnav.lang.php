7a6706ff992f6c2d5c6277df89607bb6c7f1697b
SNAKE <ethernetcat@protonmail.com>
2019-06-06 15:41:15 +0900

    Create usercpnav.lang.php

---
+++ inc/languages/japanese/usercpnav.lang.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['ucp_nav_width'] = "180";
+$l['ucp_nav_menu'] = "メニュー";
+$l['ucp_nav_messenger'] = "メッセンジャー";
+$l['ucp_nav_compose'] = "作成";
+$l['ucp_nav_tracking'] = "トラッキング";
+$l['ucp_nav_edit_folders'] = "フォルダーを編集";
+$l['ucp_nav_profile'] = "プロフィール";
+$l['ucp_nav_edit_profile'] = "プロフィールを編集";
+$l['ucp_nav_edit_options'] = "オプションを編集";
+$l['ucp_nav_change_email'] = "メールアドレスを変更";
+$l['ucp_nav_change_pass'] = "パスワードを変更";
+$l['ucp_nav_change_username'] = "ユーザー名を変更";
+$l['ucp_nav_edit_sig'] = "署名を編集";
+$l['ucp_nav_change_avatar'] = "アバターを変更";
+$l['ucp_nav_misc'] = "その他";
+$l['ucp_nav_editlists'] = "バディ/無視 リスト";
+$l['ucp_nav_favorite_threads'] = "お気に入りスレッド";
+$l['ucp_nav_subscribed_threads'] = "購読スレッド";
+$l['ucp_nav_forum_subscriptions'] = "フォーラム購読";
+$l['ucp_nav_drafts'] = "保存された下書き";
+$l['ucp_nav_drafts_active'] = "<strong>保存された下書き({1})</strong>";
+$l['ucp_nav_notepad'] = "個人用ノート";
+$l['ucp_nav_view_profile'] = "プロフィールを見る";
+$l['ucp_nav_home'] = "ユーザーCPホーム";
+$l['ucp_nav_usergroups'] = "所属グループ";
+$l['ucp_nav_attachments'] = "添付ファイル管理";
