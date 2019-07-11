737ca4851f0721294aeb82c7a9427f99d38268e6
SNAKE <ethernetcat@protonmail.com>
2019-07-11 15:43:50 +0900

    Create warnings.lang.php

---
+++ inc/languages/japanese/warnings.lang.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * MyBB 1.8 Japanese Language Pack
+ * Author: snake0053
+ * GitHub: https://github.com/snake0053/mybb-japanese
+ */
+
+$l['nav_profile'] = "{1}のプロフィール";
+$l['nav_warning_log'] = "警告ログ";
+$l['nav_add_warning'] = "ユーザーを警告";
+$l['nav_view_warning'] = "警告の詳細";
+
+$l['warning_for_post'] = "&hellip; 投稿:";
+$l['already_expired'] = "期限";
+$l['details_username'] = "ユーザー名";
+$l['warning_active'] = "有効";
+$l['warning_revoked'] = "失効";
+$l['warning_log'] = "警告ログ";
+$l['warning'] = "警告";
+$l['issued_by'] = "発行者 ";
+$l['date_issued'] = "発行日 ";
+$l['expiry_date'] = "期限 ";
+$l['active_warnings'] = "有効な警告";
+$l['expired_warnings'] = "期限切れの警告";
+$l['warning_points'] = "({1}ポイント)";
+$l['no_warnings'] = "このユーザーは警告を受け取っていないか、すべて削除されています。";
+$l['warn_user'] = "ユーザーを警告";
+$l['post'] = "投稿:";
+$l['warning_note'] = "管理メモ:";
+$l['details_warning_note'] = "管理メモ";
+$l['warning_type'] = "警告タイプ:";
+$l['custom'] = "理由を記入";
+$l['reason'] = "理由:";
+$l['points'] = "ポイント:";
+$l['details_reason'] = "理由";
+$l['warn_user_desc'] = "一つ以上のルールに違反した場合、ここからユーザーの警告レベルを引き上げることができます。";
+$l['send_pm'] = "ユーザーへ通知:";
+$l['send_user_warning_pm'] = "ユーザーに警告を伝えるプライベートメッセージを送信する。";
+$l['send_pm_subject'] = "件名:";
+$l['warning_pm_subject'] = "警告を受けました。";
+$l['send_pm_message'] = "本文:";
+$l['warning_pm_message'] = "{1}様
+
+{2}のスタッフから警告を受けました。
+--
+
+--";
+$l['send_pm_options'] = "オプション:";
+$l['send_pm_options_anonymous'] = "<strong>匿名PM</strong>: このプライベートメッセージを匿名ユーザーとして送信する。";
+$l['expiration_never'] = "永久";
+$l['expiration_hours'] = "時";
+$l['expiration_days'] = "日";
+$l['expiration_weeks'] = "週";
+$l['expiration_months'] = "月";
+$l['redirect_warned_banned'] = "<br /><br />The user has also been moved to the {1} group {2}.";
+$l['redirect_warned_suspended'] = "<br /><br />This users posting privileges have been suspended {1}.";
+$l['redirect_warned_moderate'] = "<br /><br />All posts by this user will now be moderated {1}.";
+$l['redirect_warned_pmerror'] = "<br /><br />PMは送信されませんでした。";
+$l['redirect_warned'] = "The warning level of {1} has been increased to {2}%.{3}<br /><br />You will now be taken back to where you came from.";
+$l['error_warning_system_disabled'] = "You cannot use the warning system as it has been disabled by the board administrator.";
+$l['error_cant_warn_group'] = "このグループのユーザーを警告する権限がありません。";
+$l['error_invalid_user'] = "選択したユーザーは存在しません。";
+$l['details'] = "詳細";
+$l['view'] = "見る";
+$l['current_warning_level'] = "Current warning level: <strong>{1}%</strong> ({2}/{3})";
+$l['warning_details'] = "警告の詳細";
+$l['revoke_warning'] = "この警告を失効させる";
+$l['revoke_warning_desc'] = "この警告を失効させるには理由を入力してください。警告が失効しても警告による追放や保留は取り消されません。";
+$l['warning_is_revoked'] = "警告を失効させました。";
+$l['revoked_by'] = "失効した人 ";
+$l['date_revoked'] = "失効日";
+$l['warning_already_revoked'] = "この警告はすでに失効しています。";
+$l['no_revoke_reason'] = "この警告の失効理由が入力されていません。";
+$l['redirect_warning_revoked'] = "警告が失効されユーザーの警告ポイントが減少しました。<br /><br />警告ページに戻ります。";
+$l['result'] = "結果:";
+$l['result_banned'] = "User will be moved to banned group ({1}) {2}";
+$l['result_suspended'] = "Posting privileges will be suspended {1}";
+$l['result_moderated'] = "Posts will be moderated {1}";
+$l['result_period'] = "{1} {2}";
+$l['result_period_perm'] = "永久";
+$l['hour_or_hours'] = "時";
+$l['day_or_days'] = "日";
+$l['week_or_weeks'] = "週";
+$l['month_or_months'] = "ヵ月";
+$l['expires'] = "期限:";
+$l['new_warning_level'] = "新しい警告レベル:";
+$l['error_cant_warn_user'] = "このユーザーに警告する権限がありません。";
+$l['existing_post_warnings'] = "この投稿への警告";
