ff5e1dd4bc15c3a6a03b8608f9a1050357e08478
ethernetcat <ethernetcat@protonmail.com>
2020-01-17 04:02:47 +0900

    Update datahandler_warnings.lang.php

--- inc/languages/japanese/datahandler_warnings.lang.php
+++ inc/languages/japanese/datahandler_warnings.lang.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * MyBB 1.8 English Language Pack
- * Copyright 2014 MyBB Group, All Rights Reserved
- *
+ * MyBB 1.8 Japanese Language Pack
+ * Author: SNAKE(ethernetcat)
+ * GitHub: https://github.com/ethernetcat/mybb-japanese
  */
 
-$l['warnings_reached_max_warnings_day'] = 'You cannot warn anyone as you have reached your limit on the number of warnings you can give out per day.<br /><br />The maximum number of warnings you can give out per day is {1}."';
-$l['warnings_error_invalid_user'] = "Selected user doesn't exist.";
-$l['warnings_error_invalid_post'] = "Selected post doesn't exist.";
-$l['warnings_error_cannot_warn_self'] = "You cannot add to your own warning level.";
-$l['warnings_error_user_reached_max_warning'] = "This user cannot be warned as they have already reached the maximum warning level.";
-$l['warnings_error_no_note'] = "You did not enter any administrative notes for this warning.";
-$l['warnings_error_invalid_type'] = "You have selected an invalid warning type.";
-$l['warnings_error_cant_custom_warn'] = "You do not have permission to give custom warnings to users.";
-$l['warnings_error_no_custom_reason'] = "You did not enter a reason for your custom warning.";
-$l['warnings_error_invalid_custom_points'] = "You did not enter a valid number of points to add to this users warning level. You need to enter a numeric value greater than 0 but not greater than {1}.";
-$l['warnings_error_invalid_expires_period'] = "You entered an invalid expiry type.";
+$l['warnings_reached_max_warnings_day'] = '一日の上限に達したため、は誰に警告することはできません。<br /><br />一日あたりに送信ができる警告の最大数は{1}です。"';
+$l['warnings_error_invalid_user'] = '選択されたユーザーは存在しません。';
+$l['warnings_error_invalid_post'] = '選択された投稿は存在しません。';
+$l['warnings_error_cannot_warn_self'] = '自分への警告レベルの追加はできません。';
+$l['warnings_error_user_reached_max_warning'] = 'このユーザーの警告レベルは最高レベルに達したため、警告することができません。';
+$l['warnings_error_no_note'] = '警告についての管理用メモが入力されていません。';
+$l['warnings_error_invalid_type'] = '選択された警告タイプは無効です。';
+$l['warnings_error_cant_custom_warn'] = 'ユーザーへのカスタムワーニングを作成する権限がありません。';
+$l['warnings_error_no_custom_reason'] = 'カスタムワーニングの理由が入力されていません。';
+$l['warnings_error_invalid_custom_points'] = 'ユーザーの警告レベルに追加するポイントに有効な数値が入力されていません。0以上{1}以下の数値を入力する必要があります。';
+$l['warnings_error_invalid_expires_period'] = '入力された有効期限は無効です。';
